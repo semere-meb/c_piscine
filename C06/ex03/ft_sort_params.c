2efbd0f2c3233bd867b93d629d2e49a30cf0a50f
Semere Mebrahtom <semere.meharena1@gmail.com>
2025-04-19 13:31:12 +0200

    fix: C05 reformatted

--- C06/ex03/ft_sort_params.c
+++ C06/ex03/ft_sort_params.c
@@ -1,61 +1,75 @@
 #include <unistd.h>
 
-int ft_strcmp(char *s1, char *s2) {
-    unsigned int i;
+int	ft_strcmp(char *s1, char *s2)
+{
+	unsigned int	i;
 
-    i = 0;
-    while (*s1 && *s2) {
-        if (*s1 == *s2) {
-            s1++;
-            s2++;
-            i++;
-        } else
-            return (*s1 - *s2);
-    }
-    return (*s1 - *s2);
+	i = 0;
+	while (*s1 && *s2)
+	{
+		if (*s1 == *s2)
+		{
+			s1++;
+			s2++;
+			i++;
+		}
+		else
+			return (*s1 - *s2);
+	}
+	return (*s1 - *s2);
 }
 
-void sort_args(char **argv, int count) {
-    int i;
-    int j;
-    char *temp;
+void	sort_args(char **argv, int count)
+{
+	int		i;
+	int		j;
+	char	*temp;
 
-    i = 1;
-    while (i < count - 1) {
-        j = i + 1;
-        while (j < count) {
-            if (ft_strcmp(argv[i], argv[j]) > 0) {
-                temp = argv[i];
-                argv[i] = argv[j];
-                argv[j] = temp;
-            }
-            j++;
-        }
-        i++;
-    }
+	i = 1;
+	while (i < count - 1)
+	{
+		j = i + 1;
+		while (j < count)
+		{
+			if (ft_strcmp(argv[i], argv[j]) > 0)
+			{
+				temp = argv[i];
+				argv[i] = argv[j];
+				argv[j] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
 }
 
-void write_ln(char *str) {
-    while (*str) {
-        write(1, str, 1);
-        str++;
-    }
-    write(1, "\n", 1);
+void	write_ln(char *str)
+{
+	while (*str)
+	{
+		write(1, str, 1);
+		str++;
+	}
+	write(1, "\n", 1);
 }
 
-void print_args(char **args, int count) {
-    int index;
+void	print_args(char **args, int count)
+{
+	int	index;
 
-    index = 1;
-    while (index < count) {
-        write_ln(args[index]);
-        index++;
-    }
+	index = 1;
+	while (index < count)
+	{
+		write_ln(args[index]);
+		index++;
+	}
 }
 
-int main(int argc, char **argv) {
-    if (argc > 0) {
-        sort_args(argv, argc);
-        print_args(argv, argc);
-    }
+int	main(int argc, char **argv)
+{
+	if (argc > 0)
+	{
+		sort_args(argv, argc);
+		print_args(argv, argc);
+	}
 }
