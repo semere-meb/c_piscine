3e4842fdf7c2ca85478fb49ddd29b9c35e513350
Semere Mebrahtom <semere.meharena1@gmail.com>
2025-04-19 04:47:30 +0200

    C04 done

---
+++ C05/ex08/ft_ten_queens_puzzle.c
@@ -0,0 +1,91 @@
+#include <unistd.h>
+
+void ft_putchar(char c) { write(1, &c, 1); }
+
+void ft_print_solution(char table[10][10]) {
+    int row;
+    int col;
+    int found;
+
+    row = 0;
+    while (row < 10) {
+        col = 0;
+        found = 0;
+        while (col < 10) {
+            if (table[row][col] == '1' && found == 0) {
+                ft_putchar(col + '0');
+                found = 1;
+            }
+            col++;
+        }
+        row++;
+    }
+    ft_putchar('\n');
+}
+
+int is_safe(char table[10][10], int h, int v) {
+    int i;
+    int j;
+
+    i = h - 1;
+    while (i >= 0) {
+        if (table[i][v] == '1')
+            return (0);
+        i--;
+    }
+    i = h - 1;
+    j = v - 1;
+    while (i >= 0 && j >= 0) {
+        if (table[i][j] == '1')
+            return (0);
+        i--;
+        j--;
+    }
+    i = h - 1;
+    j = v + 1;
+    while (i >= 0 && j < 10) {
+        if (table[i][j] == '1')
+            return (0);
+        i--;
+        j++;
+    }
+    return (1);
+}
+
+void solve(char table[10][10], int row, int *count) {
+    int col;
+
+    col = 0;
+    while (col < 10) {
+        if (is_safe(table, row, col)) {
+            table[row][col] = '1';
+            solve(table, row + 1, count);
+            table[row][col] = '0';
+        }
+        col++;
+    }
+    if (row == 10) {
+        ft_print_solution(table);
+        (*count)++;
+    }
+}
+
+int ft_ten_queens_puzzle(void) {
+    char table[10][10];
+    int i;
+    int j;
+    int count;
+
+    i = 0;
+    count = 0;
+    while (i < 10) {
+        j = 0;
+        while (j < 10) {
+            table[i][j] = '0';
+            j++;
+        }
+        i++;
+    }
+    solve(table, 0, &count);
+    return (count);
+}
