5446b0492c589601cc32c1acd1726ce0bcf38b08
Semere Mebrahtom <semere.meharena1@gmail.com>
2025-04-21 03:49:52 +0200

    C07 done

---
+++ C07/ex05/ft_split.c
@@ -0,0 +1,88 @@
+#include <alloca.h>
+#include <stdlib.h>
+
+int is_char_member(char c, const char *str) {
+    int i;
+
+    i = 0;
+    while (str[i]) {
+        if (str[i++] == c)
+            return (1);
+    }
+    return (0);
+}
+
+char *substr(const char *str, int start, int end) {
+    int i;
+    char *result;
+
+    if (start >= end)
+        return (NULL);
+    i = 0;
+    result = (char *)malloc((end - start + 1) * sizeof(char));
+    if (!result)
+        return (NULL);
+    while (start < end)
+        result[i++] = str[start++];
+    result[i] = '\0';
+    return (result);
+}
+
+int allocate(const char *str, const char *charset, char **array) {
+    int len;
+    int i;
+    int start;
+
+    len = 0;
+    i = 0;
+    start = -1;
+    while (str[i]) {
+        if (!is_char_member(str[i], charset)) {
+            if (start == -1)
+                start = i;
+        } else if (start != -1) {
+            array[len++] = substr(str, start, i);
+            start = -1;
+        }
+        i++;
+    }
+    if (start != -1)
+        array[len++] = substr(str, start, i);
+    return (len);
+}
+
+int array_len(const char *str, const char *charset) {
+    int len;
+    int i;
+    int start;
+
+    len = 0;
+    i = 0;
+    start = -1;
+    while (str[i]) {
+        if (!is_char_member(str[i], charset)) {
+            if (start == -1)
+                start = i;
+        } else if (start != -1) {
+            len++;
+            start = -1;
+        }
+        i++;
+    }
+    if (start != -1)
+        len++;
+    return (len);
+}
+
+char **ft_split(char *str, char *charset) {
+    int len;
+    char **array;
+
+    len = array_len(str, charset);
+    array = malloc((len + 1) * sizeof(char *));
+    if (!array)
+        return (NULL);
+    allocate(str, charset, array);
+    array[len] = NULL;
+    return (array);
+}
