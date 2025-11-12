8b541a4f6fa4e760f6d022524a2531c8c20ab18f
Semere Meharena Mebrahtom <semere.meharena1@gmail.com>
2025-11-13 00:38:53 +0100

    chore: minor change to C10/ex00

--- C10/ex00/srcs/ft_display_file.c
+++ C10/ex00/srcs/ft_display_file.c
@@ -13,15 +13,6 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-void	ft_print_str(char *str, int size)
-{
-	int	i;
-
-	i = 0;
-	while (i < size)
-		write(1, &str[i++], 1);
-}
-
 int	ft_display_file(char *file_name)
 {
 	char	buf[10];
@@ -35,7 +26,7 @@ int	ft_display_file(char *file_name)
 	while (size > 0)
 	{
 		size = read(fd, buf, 10);
-		ft_print_str(buf, size);
+		write(1, buf, size);
 	}
 	return (size);
 }
